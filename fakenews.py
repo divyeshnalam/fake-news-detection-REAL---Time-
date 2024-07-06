83ad48c488ee4c90bc0623f7551fe655bc0c3b6d
Divyesh Nalam <116959406+divyeshnalam@users.noreply.github.com>
2024-07-06 19:24:21 +0530

    Add files via upload

---
+++ fakenews.py
@@ -0,0 +1,89 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_news_from_website(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    articles = []
+    for article in soup.find_all('div', class_='article'):
+        title = article.find('h2').get_text()
+        content = article.find('p').get_text()
+        articles.append({'title': title, 'content': content})
+    
+    return articles
+
+
+url = 'https://newsapi.org/v2/everything?q=Apple&from=2024-07-06&sortBy=popularity&apiKey=API_KEY'
+articles = fetch_news_from_website(url)
+
+
+
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+
+# pre-trained model and tokenizer
+model_name = 'mrm8488/bert-tiny-finetuned-fake-news-detection'
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name)
+
+def predict_fake_news(text):
+    if text is None:
+        return "Unknown"
+    
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    
+    # model predictions
+    with torch.no_grad():
+        outputs = model(**inputs)
+        predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+        predicted_label = torch.argmax(predictions, dim=1).item()
+    
+    # prediction
+    return "Fake" if predicted_label == 1 else "Real"
+
+
+import streamlit as st
+import requests
+
+def fetch_news_from_api(api_key):
+    url = 'https://newsapi.org/v2/top-headlines'
+    params = {
+        'apiKey': api_key,
+        'country': 'us',
+        'pageSize': 50 
+    }
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            articles = [{'title': article['title'], 'content': article['description']} for article in data['articles']]
+            return articles
+        else:
+            print(f'Error fetching news: {response.text}')
+            return [] 
+    except Exception as e:
+        print(f'Exception during news fetching: {str(e)}')
+        return []  
+    
+st.title("Real-Time Fake News Detection with BERT")
+
+def fetch_and_display_news():
+    st.subheader("Latest News Articles")
+    
+    api_key = '484aad51e72d46d9b450332fbfbd38c3'
+    articles = fetch_news_from_api(api_key)
+    
+    if articles:
+        for article in articles:
+            st.write(f"**Title:** {article['title']}")
+            content = article.get('content')
+            st.write(f"**Content:** {content}")
+            prediction = predict_fake_news(content)
+            st.write(f"**Prediction:** {prediction}")
+            st.write("---")
+    else:
+        st.write("No articles fetched. Check your API key or website scraping function.")
+
+if st.button("Fetch News"):
+    fetch_and_display_news()
